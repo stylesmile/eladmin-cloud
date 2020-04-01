5105ac74231beba0b88108b8f0fd7737d03b713e
chenye <15002770045@139.com>
2020-04-01 21:20:42 +0800

    gateway

---
+++ sunny-gateway/src/main/java/com/sunny/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.sunny.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Stylesmile
+ * @date 2020-0401
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
