90a207cd3d8ef41118ffeaa574f2f6fc8f595250
chenye <15002770045@139.com>
2020-04-01 21:19:03 +0800

    注册中心

---
+++ sunny-regist/src/main/java/com/sunny/regist/RegistApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.regist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistApplication.class, args);
+    }
+
+}
