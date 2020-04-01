cb9a1c3fbc15c928aeca740227a475b579736e81
chenye <15002770045@139.com>
2020-04-01 23:32:21 +0800

    eureka自我注册

--- sunny-regist/src/main/java/com/sunny/regist/RegistApplication.java
+++ sunny-regist/src/main/java/com/sunny/regist/RegistApplication.java
@@ -2,7 +2,13 @@ package com.sunny.regist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author chenye
+ * @date 2020-0401
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class RegistApplication {
 
